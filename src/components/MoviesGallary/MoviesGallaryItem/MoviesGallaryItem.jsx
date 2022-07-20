0c8a9478bcc1e465d344d7023505df093d6f5127
Volodymyr Romanenko <romanonebe@gmail.com>
2022-07-20 16:20:00 +0300

    Update MoviesGallaryItem.jsx

--- src/components/MoviesGallary/MoviesGallaryItem/MoviesGallaryItem.jsx
+++ src/components/MoviesGallary/MoviesGallaryItem/MoviesGallaryItem.jsx
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 export const MoviesGallaryItem = ({ id, image, title, rate, onDelete }) => {
   return (
     <li>
-      <img src={`https://image.tmdb.org/t/p/w500${image}`} width="350" />
+      <img
+        src={`https://image.tmdb.org/t/p/w500${image}`}
+        width="350"
+        alt={title}
+      />
       <h2>{title}</h2>
       <p>Votes: {rate}</p>
       <button type="button" onClick={() => onDelete(id)}>
