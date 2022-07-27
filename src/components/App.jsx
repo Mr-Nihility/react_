557833e97017cc7e06e6e0328c8cb706c7f85a4c
Volodymyr Romanenko <romanonebe@gmail.com>
2022-07-27 15:14:52 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 // import Section from './section/Section';
 // import { FeaturesGallary } from './featuresGallary/FeaturesGallary';
 import { mapper } from 'utils/mapper';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from './Button/Button';
 import { MoviesGallary } from './MoviesGallary/MoviesGallary';
 import { Modal } from './Modal/Modal';
@@ -11,97 +11,174 @@ import { Loader } from './Loader/Loader';
 // import data from '../data/features.json';
 //-------------------------------------------------------///
 
-class App extends Component {
-  state = {
-    films: [],
-    isShown: false,
-    image: '',
-    page: 1,
-    loading: false
+const App = () => {
+  const [films, setFilms] = useState([]);
+  // const [isShown, setIsShown] = useState(false);
+  const [image, setImage] = useState('');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    service(page)
+      .then(({ data }) => {
+        setFilms(prev => [...prev, ...mapper(data.results)]);
+      })
+      .finally(() => setLoading(false));
+  }, [page]);
+
+  const handlerClick = () => {
+    // setIsShown(true);
+    fetchMovies();
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.page !== this.state.page) {
-      this.fetchMovies()
-    }
-  }
-  
-  fetchMovies = () => {
-    this.setState({loading: true})
-    service(this.state.page).then(({data}) => {this.setState((ps) =>({films : [...ps.films, ...mapper(data.results)]}))})
-    .finally(() => this.setState({loading: false}))
-  }
-
-  handleOpenModal = (img) => {
-  this.setState({image:img})
-  }
-  handleCloseModal = () => {
-  this.setState({image:''})
-  }
-
-  handlerClick = () => {
-    this.setState({ isShown: true });
-    this.fetchMovies()
-  };
-  onDelete = id => {
-    this.setState(prevState => ({
-      films: prevState.films.filter(item => item.id !== id),
-    }));
+  const fetchMovies = () => {
+    setLoading(true);
+    service(page)
+      .then(({ data }) => {
+        setFilms(prev => [...prev, ...mapper(data.results)]);
+      })
+      .finally(() => setLoading(false));
   };
 
-  handlerLoadMore = () => {
-    this.setState((ps) => ({page: ps.page + 1 }))
-  }
+  const handleOpenModal = img => {
+    setImage(img);
+  };
+  const handleCloseModal = () => {
+    setImage('');
+  };
 
-  onToggleStatus = filmId => {
-    
-    this.setState(prev => ({
-      
-      films: prev.films.map(item => {
-        
+  const onToggleStatus = filmId => {
+    setFilms(prev =>
+      prev.map(item => {
         if (item.id === filmId) {
-          
-          return {...item, watched: !item.watched}
-          
+          return { ...item, watched: !item.watched };
         }
-        return item
-      }),
-    }));
+        return item;
+      })
+    );
   };
 
-  render() {
-    const { isShown, films,image, loading } = this.state;
-    return (
-      <div>
-        {!isShown && (
-          <Button
-            textContent="Show something"
-            handlerClick={this.handlerClick}
-          />
-        )}
+  const onDelete = id => {
+    setFilms(prev => prev.filter(item => item.id !== id));
+  };
+  const handlerLoadMore = () => {
+    setPage(ps => ps + 1);
+  };
 
-        {isShown && (
-          <>
-          <MoviesGallary
-            handleOpenModal={this.handleOpenModal}
-            onToggleStatus={this.onToggleStatus}
-            films={films}
-            onDelete={this.onDelete}
-          />
-          {loading && (<Loader/>)}
-          <Button
-            textContent="Load More"
-            handlerClick={this.handlerLoadMore}
-          />
-          </>
-        )}
-        {image && (
-          <Modal image={image} close={this.handleCloseModal } />
-        )}
-        
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {/* {!isShown && (
+        <Button textContent="Show something" handlerClick={handlerClick} />
+      )} */}
+
+      {/* {isShown && ( */}
+      <>
+        <MoviesGallary
+          handleOpenModal={handleOpenModal}
+          onToggleStatus={onToggleStatus}
+          films={films}
+          onDelete={onDelete}
+        />
+        {/* {loading && <Loader />} */}
+        <Button textContent="Load More" handlerClick={handlerLoadMore} />
+      </>
+      {/* )} */}
+      {image && <Modal image={image} close={handleCloseModal} />}
+    </div>
+  );
+};
+
+// class App extends Component {
+//   state = {
+//     films: [],
+//     isShown: false,
+//     image: '',
+//     page: 1,
+//     loading: false
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if(prevState.page !== this.state.page) {
+//       this.fetchMovies()
+//     }
+//   }
+
+//   fetchMovies = () => {
+//     this.setState({loading: true})
+//     service(this.state.page).then(({data}) => {this.setState((ps) =>({films : [...ps.films, ...mapper(data.results)]}))})
+//     .finally(() => this.setState({loading: false}))
+//   }
+
+//   handleOpenModal = (img) => {
+//   this.setState({image:img})
+//   }
+//   handleCloseModal = () => {
+//   this.setState({image:''})
+//   }
+
+//   handlerClick = () => {
+//     this.setState({ isShown: true });
+//     this.fetchMovies()
+//   };
+//   onDelete = id => {
+//     this.setState(prevState => ({
+//       films: prevState.films.filter(item => item.id !== id),
+//     }));
+//   };
+
+//   handlerLoadMore = () => {
+//     this.setState((ps) => ({page: ps.page + 1 }))
+//   }
+
+//   onToggleStatus = filmId => {
+
+//     this.setState(prev => ({
+
+//       films: prev.films.map(item => {
+
+//         if (item.id === filmId) {
+
+//           return {...item, watched: !item.watched}
+
+//         }
+//         return item
+//       }),
+//     }));
+//   };
+
+//   render() {
+//     const { isShown, films,image, loading } = this.state;
+//     return (
+//       <div>
+//         {!isShown && (
+//           <Button
+//             textContent="Show something"
+//             handlerClick={this.handlerClick}
+//           />
+//         )}
+
+//         {isShown && (
+//           <>
+//           <MoviesGallary
+//             handleOpenModal={this.handleOpenModal}
+//             onToggleStatus={this.onToggleStatus}
+//             films={films}
+//             onDelete={this.onDelete}
+//           />
+//           {loading && (<Loader/>)}
+//           <Button
+//             textContent="Load More"
+//             handlerClick={this.handlerLoadMore}
+//           />
+//           </>
+//         )}
+//         {image && (
+//           <Modal image={image} close={this.handleCloseModal } />
+//         )}
+
+//       </div>
+//     );
+//   }
+// }
 
 export { App };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,32 +1,76 @@
-import { Component } from "react";
-import { Backdrop,ModalDiv,ImageStyle } from "./Modal.styles";
+import { useEffect, useState } from 'react';
+import { Backdrop, ModalDiv, ImageStyle } from './Modal.styles';
 
-export class Modal extends Component{
+export const Modal = ({ close, image }) => {
+  const [loaded, setLoaded] = useState(false);
 
-    componentDidMount() {
-    window.addEventListener('keydown', this.handleCloseByEsc)
-    }
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleCloseByEsc)
-    }
-    handleCloseByEsc = event => {
-        if (event.code === 'Escape') {
-            this.props.close()
-        }
+  useEffect(() => {
+    const handleCloseByEsc = e => {
+      if (e.code === 'Escape') {
+        close();
+      }
     };
-    
-    render() {
-        const { image,close } = this.props;
-        return (
-            <Backdrop>
-                <ModalDiv>
-                    <ImageStyle src={`https://image.tmdb.org/t/p/w500${image}`}
-        width="350"
-                        alt="Picture" />
-                    <button type="button" onClick={close}>x</button>
-            </ModalDiv>
-           </Backdrop> 
-        )
+    window.addEventListener('keydown', handleCloseByEsc);
+    return () => {
+      window.removeEventListener('keydown', handleCloseByEsc);
+    };
+  });
+  const loadedHandler = () => {
+    setLoaded(true);
+  };
+  return (
+    <Backdrop>
+      <ModalDiv>
+        <ImageStyle
+          src={`https://image.tmdb.org/t/p/w500${image}`}
+          width="350"
+          alt="Picture"
+          onLoad={loadedHandler}
+        />
+        {!loaded && (
+          <h3
+            style={{
+              fontSize: 50,
+              color: 'white',
+            }}
+          >
+            Loading...
+          </h3>
+        )}
+        <button type="button" onClick={close}>
+          x
+        </button>
+      </ModalDiv>
+    </Backdrop>
+  );
+};
 
-    }
-}
+// export class Modal extends Component{
+
+//     componentDidMount() {
+//     window.addEventListener('keydown', this.handleCloseByEsc)
+//     }
+//     componentWillUnmount() {
+//         window.removeEventListener('keydown', this.handleCloseByEsc)
+//     }
+//     handleCloseByEsc = event => {
+//         if (event.code === 'Escape') {
+//             this.props.close()
+//         }
+//     };
+
+//     render() {
+//         const { image,close } = this.props;
+//         return (
+//             <Backdrop>
+//                 <ModalDiv>
+//                     <ImageStyle src={`https://image.tmdb.org/t/p/w500${image}`}
+//         width="350"
+//                         alt="Picture" />
+//                     <button type="button" onClick={close}>x</button>
+//             </ModalDiv>
+//            </Backdrop>
+//         )
+
+//     }
+// }
